edf2b6d70772d58516f2db40d0a679bb4297dcef
PATUGANSU <pedrohenriqueboneti@gmail.com>
2024-10-15 12:05:52 +0000

    Array Class 1

---
+++ array.ts
@@ -0,0 +1,49 @@
+var numberlist: number[] = [2, 5, 7, 11, 3] // creates an array
+
+numberlist.push (12, 15) // adds a value to the end of the array
+console.log (numberlist)
+
+numberlist.pop () // removes the last value of the array
+console.log (numberlist)
+
+numberlist.shift () // removes the first value of the array
+console.log (numberlist)
+
+numberlist.unshift (1) // adds a value to the beginning of the array
+console.log (numberlist)
+
+console.log (numberlist.length) // shows the length of an array
+
+// Supermarket Queue
+// customers: "Jurema", "Arnoldo", "Rodolfo", "Janetina", "Mario" 
+
+var namelist: string[] = []
+namelist.unshift ("Jurema")
+console.log (namelist)
+
+namelist.unshift ("Arnoldo")
+console.log (namelist)
+
+namelist.unshift ("Rodolfo")
+console.log (namelist)
+
+namelist.unshift ("Janetina")
+console.log (namelist)
+
+namelist.unshift ("Mario")
+console.log (namelist)
+
+namelist.pop ()
+console.log (namelist)
+
+namelist.pop ()
+console.log (namelist)
+
+namelist.pop ()
+console.log (namelist)
+
+namelist.pop ()
+console.log (namelist)
+
+namelist.pop ()
+console.log (namelist)
